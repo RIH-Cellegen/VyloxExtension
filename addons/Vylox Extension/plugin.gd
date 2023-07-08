61ea51446a482fd5f052714d7e4eeb11e3a0b0da
Cellegen <67655961+RIH-Cellegen@users.noreply.github.com>
2023-07-08 12:21:03 +0200

    Updating Plugin functionality for 0.1.a.3
    The current script is capable of safely adding / erasing stuff (Autoloaded singletons or scenes) without any conflict. It'll store any number of actions taken, as well as erasing that many number of actions, when plugin's disabled.

--- addons/Vylox Extension/plugin.gd
+++ addons/Vylox Extension/plugin.gd
@@ -1,14 +1,83 @@
 @tool
 extends EditorPlugin
 
+enum plugin_load {Loading, Loaded, Deloaded}
+
+# vextension_tab is loaded by checking if the original folder's absolute path is resolved. If it didn't resolve,
+# it'll return to null, which means that the file inside this Directory doesn't exist.
+var vextension_tab = load(FileAccess.open("res://addons/Vylox Extension/Core/Vylox Extension Tab/Vylox Extension Tab.tscn", FileAccess.READ).get_path_absolute())
+var instanced_vextension_tab: Control
+
+var vscript_path = FileAccess.open("res://addons/Vylox Extension/Core/VScript/VScript.gd", FileAccess.READ).get_path()
+var vevent_path = FileAccess.open("res://addons/Vylox Extension/Core/VEvent/VEvent.gd", FileAccess.READ).get_path()
+
+var plugin_starts: int = plugin_load.Deloaded
+var count_loads: int = 0
+var max_loads: int = 4
+var plugin_loaded_functions: Array = []
 
 func _enter_tree():
-	add_autoload_singleton("VScript", "Core/VScript/VScript.gd")
-	add_autoload_singleton("VInput", "Core/VInput/Vinput.gd")
-	add_autoload_singleton("VEvent", "Core/VEvent/VEvent.gd")
+	if vextension_tab != null: instanced_vextension_tab = vextension_tab.instantiate()
+	plugin_starts = plugin_load.Loading
 
+func _process(delta):
+	# Plugin Autoloading System: This system will make sure, that Autoloaded scripts, which
+	# extend to another Autoloaded script will load properly and in order to not print errors
+	# and let the scripts work properly after Autoloading them.
+	
+	# For faster computing, if it's already on the Loaded state, we just return it, no
+	# need for further calculation.
+	if plugin_starts == plugin_load.Loaded:
+		return
+	
+	# Resizing the Array to an appropriate size.
+	if plugin_loaded_functions.size() != max_loads: plugin_loaded_functions.resize(max_loads)
+	
+	# If if it reached the max amount of loads, then set it to Loaded state.
+	# If not, then for each progress frame, it loads all functions, in order,
+	# matching with "count_loads". Some functions don't run, if the path doesn't exist
+	# and those will not be stored on "plugin_loaded_functions" array.
+	if plugin_starts == plugin_load.Loading:
+		if count_loads == max_loads:
+			plugin_starts = plugin_load.Loaded
+			return
+		if count_loads < max_loads and plugin_loaded_functions[count_loads] == null and vscript_path != null:
+			match count_loads:
+				# Replace the functions with any function you want.
+				# Numbers indicate the amount of codes / functions running. Once it reaches
+				# the maximum amount of loads (as in, reaching "max_loads" value),
+				# plugin load will be set to Loaded state.
+				0: 
+					add_autoload_singleton("VScript", vscript_path)
+					plugin_loaded_functions[count_loads] = "VScript"
+					count_loads += 1
+					return
+				1: 
+					pass # Empty space
+				2: 
+					if vevent_path != null:
+						add_autoload_singleton("VEvent", vevent_path)
+						plugin_loaded_functions[count_loads] = "VEvent"
+					count_loads += 1
+					return
+				3: 
+					if instanced_vextension_tab != null:
+						add_control_to_bottom_panel(instanced_vextension_tab, "VInput")
+						plugin_loaded_functions[count_loads] = "Vylox Extension Tab"
+					count_loads += 1
+					return
 
 func _exit_tree():
-	remove_autoload_singleton("VScript")
-	remove_autoload_singleton("VInput")
-	remove_autoload_singleton("VEvent")
+	# Plugin Autoloading System: On exiting the tree, process() function will no longer run,
+	# so this code will reset the plugin load state (Deloaded) and deload only the functions
+	# with the corresponding number.
+	if plugin_starts == plugin_load.Loaded:
+		for i in max_loads:
+			if plugin_loaded_functions[i] != null:
+				plugin_loaded_functions[i] = null
+				match i:
+					0: remove_control_from_bottom_panel(instanced_vextension_tab)
+					1: remove_autoload_singleton("VEvent")
+					2: remove_autoload_singleton("VInput")
+					3: remove_autoload_singleton("VScript")
+		plugin_starts = plugin_load.Deloaded

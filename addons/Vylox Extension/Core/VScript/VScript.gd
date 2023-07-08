e75a1873ddf1a73d31cb0485f3977b3741c48a59
Cellegen <67655961+RIH-Cellegen@users.noreply.github.com>
2023-07-08 12:28:37 +0200

    Update VScript for 0.1.a.3
    Changed function's names:
    - quaternion_2_vector4() -> quaternion2vector4()
    - vector4_2_quaternion() -> vector42quaternion()
    
    Added functions:
    - distance() -> Calculate the length of 2 positions and return the length
    _ variable_equals_to() -> It has two checks, one is if the two variables are the same type, and the other is if the two variables have the same value. This will make it, so that we are checking if the 2 variables are completely the same in both Type and Value.

--- addons/Vylox Extension/Core/VScript/VScript.gd
+++ addons/Vylox Extension/Core/VScript/VScript.gd
@@ -1,6 +1,5 @@
 @tool
 extends Node
-class_name VScript
 
 func clamp_vector2(value: Vector2, min_val: Variant, max_val: Variant):
 	value.x = clamp(value.x, min_val, max_val)
@@ -20,10 +19,10 @@ func clamp_vector4(value: Vector4, min_val: Variant, max_val: Variant):
 	value.z = clamp(value.w, min_val, max_val)
 	return value
 
-func quaternion_2_vector4(value: Quaternion) -> Vector4:
+func quaternion2vector4(value: Quaternion) -> Vector4:
 	return Vector4(value.x, value.y, value.z, value.w)
 
-func vector4_2_quaternion(value: Vector4) -> Quaternion:
+func vector42quaternion(value: Vector4) -> Quaternion:
 	return Quaternion(value.x, value.y, value.z, value.w)
 
 # Used in VInspector. It turns all of the enum's keys into a String, containing keys as Strings.
@@ -49,3 +48,15 @@ func enum2str(enum_dict: Dictionary, include_values := false, capitalize_words :
 	if enum_string.length() > 0:
 		enum_string = enum_string.substr(0, enum_string.length() - 1)
 	return enum_string
+
+func distance(a: Variant, b: Variant):
+	if (a is int and b is int) or (a is float and b is float):
+		return abs(a - b)
+	if a is Vector2 and b is Vector2:
+		return Vector2(abs(a.x - b.x), abs(a.y - b.y))
+	
+	return 0
+
+func variable_equals_to(variable, to): ## Checks if the parameters are either the same type or same value and returns the result.
+	if typeof(variable) != typeof(to): return false # Check if the "variable" is the same type as "to"
+	return true if variable == to else false # And if they are, then see if it's the same value
